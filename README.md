40b062870f3274b9987df5b3d0b1e2c0fa36fdd5
Grégory Paul <paulgreg@gmail.com>
2014-12-14 17:19:50 +0100

    Add SMS notification, README, check interval

---
+++ README.md
@@ -0,0 +1,7 @@
+=== CalDAV SMS notifier ===
+
+That node programs fetch calendars from a CalDAV server, get next events and, for all of thoses, send an SMS as reminder.
+
+The SMS notification service is the one my mobile service providing is offering, using a simple HTTP request (http://www.nextinpact.com/news/88097-avec-free-mobile-vos-appareils-connectes-peuvent-vous-envoyer-sms.htm).
+
+Tested with a [http://baikal-server.com/](Baikal server).
--- index.js
+++ index.js
@@ -1,23 +1,55 @@
+var config = require("./config.js");
 var caldav = require("node-caldav");
 var moment = require("moment");
-var config = require("./config.js");
 
-caldav.getList(config.baseurl + config.baikal, config.username, config.password, function(out) {
-    out.map(function(calendar) {
+var debug = false;
+
+caldav.getList(config.baseurl + config.baikal, config.username, config.password, function(calendars) {
+    calendars.map(function(calendar) {
+
         if (calendar.displayName !== '') {
-            caldav.getEvents(config.baseurl + calendar.href, config.username, config.password, function(events) {
+
+            var from = moment().format('YYYYMMDDTHHmmss');
+            var end  = moment().minutes(config.checkIntervalInMinutes).format('YYYYMMDDTHHmmss');
+
+            caldav.getEvents(config.baseurl + calendar.href, config.username, config.password, from, end, function(events) {
+
                 events.map(function(event) {
                     var d;
                     for(var key in event) {
                         if (key.indexOf('DTSTART') === 0) {
                             var start = event[key];
-                            // console.log(key, start);
+                            if (debug) { console.log(key, start); }
                             d = moment(start, ['YYYYMMDD', 'YYYYMMDDTHHmmss'] );
                         }
                     }
 
-                    //console.log(event);
-                    console.log('calendar', calendar.displayName, d.format(), ' : ', event.SUMMARY);
+                    if (debug) { console.log(event); }
+
+                    var message = "{date}{time} {title} ({calendar})"
+                    .replace("{date}", d.format('DD/MM'))
+                    .replace("{time}", (d.hours() !== 0 && d.minutes() !== 0) ? " " + d.format('HH:mm') : "")
+                    .replace("{title}", event.SUMMARY)
+                    .replace("{calendar}", calendar.displayName);
+
+                    if (debug) { console.log(message); }
+
+                    var smsUrl = "https://smsapi.free-mobile.fr/sendmsg?user={username}&pass={password}&msg={message}"
+                    .replace("{username}", config.smsUsername)
+                    .replace("{password}", config.smsPassword)
+                    .replace("{message}", encodeURIComponent(message));
+
+                    if (debug) { console.log(smsUrl); }
+
+                    var request = require('request');
+                    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+                    request(smsUrl, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            if (debug) { console.log('ok'); }
+                        } else if (error || (response && response.statusCode !== 200)) {
+                            console.error(error, " - ", (response) ? response.statusCode : "no response");
+                        }
+                    })
                 });
             });
         }
